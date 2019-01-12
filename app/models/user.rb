f6d74896fd95656ccb160115d9cea99c14b63320
Najib Adan <najibadan1@gmail.com>
2019-01-12 11:20:41 +0300

    Created users

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :username, presence: true, uniqueness: { case_sensitive: false }, length: {maximum: 50}
+  validates :email, presence: true, length: {maximum: 255}
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'static_pages#home'
+  get 'home', to: 'static_pages#home'
+  get 'contact', to: 'staticpages#contact'
+  get 'about', to: 'staticpages#about'
+  resources :users
 end
