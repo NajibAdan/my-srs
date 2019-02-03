8017817e1c19c6d0341704863a45d9465a112794
Najib Adan <najibadan@airmail.cc>
2019-02-03 10:17:44 +0300

    Added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,42 @@
-User.create(
-    name: Faker::Name,
-    email: Faker::Internet.email,
-    password: 'password', 
+#Creates dummy users
+User.create!(
+    name: 'Foo Bar',
+    email: 'foo@bar.com',
+    password: 'password',
     password_confirmation: 'password'
 )
+5.times do 
+    User.create!(
+        name: Faker::Name,
+        email: Faker::Internet.email,
+        password: 'password', 
+        password_confirmation: 'password'
+    )
+end
+
+#Creates a dummy deck for User 1
+user = User.first
+3.times do 
+    user.decks.create!(
+        name: Faker::SwordArtOnline.item,
+        description: Faker::TheITCrowd.quote
+    )
+end
+
+# Fill the dummy decks with dummy values
+user.decks.count do |deck|
+    4.times do
+        random_day = Date.today + rand(0..2)
+        card = deck.cards.create!(
+            day_to_study: random_day.strftime("%d/%m/%Y")
+        )
+        rand(1..4).times do
+            card.fronts.create!(
+                text_field: Faker::LeagueOfLegends.quote
+            )
+            card.backs.create!(
+                text_field: Faker::LeagueOfLegends.champion
+            )
+        end
+    end
+end
