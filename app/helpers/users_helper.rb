daf7cecd9b5c49d92c30cb624c4d55a373a0a05d
Najib Adan <najibadan@airmail.cc>
2019-02-15 11:08:27 +0300

    Added filters to prevent editing and deleting other users

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,7 +3,17 @@ module UsersHelper
     def admin_only
         if !current_user.admin?
             flash[:notice] = "Admins only"
-            redirect_to current_user
+            redirect_to user_session_url
         end
+    end
+
+    def user_params
+        params.require(:user).permit(:name, :email, :password,
+        :password_confirmation)
+    end
+
+    def correct_user
+        @user =  User.find(params[:id])
+        redirect_to root_url unless (@user==current_user || current_user.admin?)
     end
 end
